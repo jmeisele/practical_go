444fcfbbe108e2e545a0596c71bcee787547843f
jmeisele <jmeisele@yahoo.com>
2022-04-26 11:00:10 -0500

    :construction:

--- files/main.go
+++ files/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello from files.go")
+	// Files
+}
+func killServer(pidFile string) error {
+	// os.Open - read
+	// os.Write - write & truncate
+	// os.OpenFile - append (and more)
+	// Go idiom: acquire resource, check for error, defer release
+	file, err := os.Open(pidFile)
+	if err != nil {
+		return err
+	}
+	// defer handles resources, file.Close() will be called when we exit killServer
+	defer file.Close()
+
+	var pid int
+	if _, err := fmt.Fscanf(file, "%d", &pid); err != nil {
+		return err
+	}
 }
